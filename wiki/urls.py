0bd941afd28b57d35d4c0d24b731f4d4258c0c86
woonerb <woonerb@daum.net>
2026-04-05 00:11:14 +0900

    에디터 구현 완료

---
+++ wiki/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('write/', views.post_create, name='post_create'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+]
--- wiki/views.py
+++ wiki/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from .models import Post
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content') # 에디터의 HTML 데이터
+        
+        if title and content:
+            # DB에 저장
+            post = Post.objects.create(
+                title=title, 
+                content=content, 
+                author=request.user
+            )
+            return redirect('post_detail', pk=post.pk) # 저장 후 상세 페이지로 이동
+            
+    return render(request, 'wiki/editor.html')
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'wiki/detail.html', {'post': post})
